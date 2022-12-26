f5e7a8d36d113240bb06f41d0b138596fbaf3296
arup-paul <aruppaulctg@gmail.com>
2022-12-26 17:29:31 +0600

    user crud api

--- react-spa/src/views/Users.jsx
+++ react-spa/src/views/Users.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axiosClient from "../axios-client.js";
 
 const Users = () => {
+    const [users,setUsers] = useState([]);
+    const [loading,setLoading] = useState(false);
+
+    useEffect(() => {
+         getUsers()
+    },[])
+
+    const getUsers = () => {
+        setLoading(true)
+        axiosClient.get('/users')
+            .then(({data}) => {
+                setLoading(false)
+                console.log(data)
+            }).catch(() => {
+               setLoading(false)
+         })
+    }
+
     return (
         <div>
             users
